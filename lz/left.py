cf519dc8fe7709cb492ddbeafcc52e9fb4a2354c
Ibrakov Azat <azatibrakov@gmail.com>
2018-09-21 17:18:07 +0300

    Add modules for left/right specific functions & functions factories

---
+++ lz/left.py
@@ -0,0 +1,35 @@
+import functools
+import itertools
+from typing import (Callable,
+                    Iterable)
+
+from lz.base import expand
+from lz.hints import (Domain,
+                      Map,
+                      Range)
+
+
+def accumulator(function: Callable[[Range, Domain], Range],
+                initial: Range
+                ) -> Map[Iterable[Domain], Iterable[Range]]:
+    attach_initial = attacher(initial)
+
+    def accumulate(iterable: Iterable[Domain]) -> Iterable[Range]:
+        yield from itertools.accumulate(attach_initial(iterable), function)
+
+    return accumulate
+
+
+def attacher(object_: Domain) -> Map[Iterable[Domain], Iterable[Domain]]:
+    def attach(iterable: Iterable[Domain]) -> Iterable[Domain]:
+        yield from itertools.chain(expand(object_), iterable)
+
+    return attach
+
+
+def folder(function: Callable[[Range, Domain], Range],
+           initial: Range) -> Map[Iterable[Domain], Range]:
+    def fold(iterable: Iterable[Domain]) -> Range:
+        return functools.reduce(function, iterable, initial)
+
+    return fold
---
+++ lz/right.py
@@ -0,0 +1,32 @@
+import itertools
+from typing import (Callable,
+                    Iterable)
+
+from lz import left
+from lz.base import (expand,
+                     reverse)
+from lz.functional import (compose,
+                           flip)
+from lz.hints import (Domain,
+                      Map,
+                      Range)
+
+
+def accumulator(function: Callable[[Domain, Range], Range],
+                initial: Range
+                ) -> Map[Iterable[Domain], Iterable[Iterable[Range]]]:
+    left_accumulator = left.accumulator(flip(function), initial)
+    return compose(left_accumulator, reverse)
+
+
+def attacher(object_: Domain) -> Map[Iterable[Domain], Iterable[Domain]]:
+    def attach(iterable: Iterable[Domain]) -> Iterable[Domain]:
+        yield from itertools.chain(iterable, expand(object_))
+
+    return attach
+
+
+def folder(function: Callable[[Domain, Range], Range],
+           initial: Range) -> Map[Iterable[Domain], Range]:
+    left_folder = left.folder(flip(function), initial)
+    return compose(left_folder, reverse)
