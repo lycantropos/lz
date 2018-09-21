608530086da0c0625a580f30d9d86c8fd33148f1
Ibrakov Azat <azatibrakov@gmail.com>
2018-09-21 17:15:28 +0300

    Add helper functions

---
+++ lz/functional.py
@@ -0,0 +1,63 @@
+import functools
+import operator
+from typing import (Any,
+                    Callable,
+                    Tuple,
+                    TypeVar)
+
+from .hints import (Domain,
+                    Map,
+                    Predicate,
+                    Range)
+
+
+def identity(argument: Domain) -> Domain:
+    return argument
+
+
+def compose(*maps: Map) -> Map[Domain, Any]:
+    Intermediate = TypeVar('Intermediate')
+
+    def binary_compose(left_map: Map[Intermediate, Range],
+                       right_map: Map[Domain, Intermediate]
+                       ) -> Map[Domain, Range]:
+        def composition(argument: Domain) -> Any:
+            return left_map(right_map(argument))
+
+        return composition
+
+    return functools.reduce(binary_compose, maps)
+
+
+def combine(*maps: Map) -> Map[Tuple[Domain, ...],
+                               Tuple[Range, ...]]:
+    def combined(arguments: Tuple[Domain, ...]) -> Tuple[Range, ...]:
+        if len(arguments) != len(maps):
+            raise ValueError('There should be {count} arguments '
+                             'for each of maps, '
+                             'but found {actual_count}.'
+                             .format(count=len(maps),
+                                     actual_count=len(arguments)))
+        return tuple(map_(argument)
+                     for map_, argument in zip(maps, arguments))
+
+    return combined
+
+
+def to_constant(object_: Domain) -> Callable[..., Domain]:
+    def constant(*_: Domain, **__: Domain) -> Domain:
+        return object_
+
+    return constant
+
+
+def flip(function: Callable[..., Range]) -> Callable[..., Range]:
+    @functools.wraps(function)
+    def flipped(*args, **kwargs):
+        return function(*reversed(args), **kwargs)
+
+    return flipped
+
+
+def negate(predicate: Predicate) -> Predicate:
+    return compose(operator.not_, predicate)
