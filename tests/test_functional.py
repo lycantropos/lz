4a2c64f401599b99f4d37b4612b2497c24afd648
Ibrakov Azat <azatibrakov@gmail.com>
2018-09-28 14:37:04 +0300

    Separate preparation/run/assertions

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -8,17 +8,24 @@ from lz.hints import Predicate
 
 
 def test_identity(object_: Any) -> None:
-    assert identity(object_) is object_
+    result = identity(object_)
+
+    assert result is object_
 
 
 def test_negate(false_predicate: Predicate,
                 true_predicate: Predicate,
                 positional_arguments: Tuple,
                 keyword_arguments: Dict[str, Any]) -> None:
-    negated_false_predicate = negate(false_predicate)
-    negated_true_predicate = negate(true_predicate)
+    non_false_predicate = negate(false_predicate)
+    non_true_predicate = negate(true_predicate)
 
-    assert negated_false_predicate(*positional_arguments,
-                                   **keyword_arguments)
-    assert not negated_true_predicate(*positional_arguments,
-                                      **keyword_arguments)
+    non_false_predicate_result = non_false_predicate(*positional_arguments,
+                                                     **keyword_arguments)
+    non_true_predicate_result = non_true_predicate(*positional_arguments,
+                                                   **keyword_arguments)
+
+    assert isinstance(non_false_predicate_result, bool)
+    assert isinstance(non_true_predicate_result, bool)
+    assert non_false_predicate_result
+    assert not non_true_predicate_result
