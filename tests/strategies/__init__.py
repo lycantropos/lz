97c3f3ee4adfefe5425624fa8a646f920fff8a6a
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-03 14:08:12 +0300

    Restructurize imports

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -1,10 +1,10 @@
+from .definitions import (built_in_functions,
+                          functions,
+                          methods,
+                          methods_descriptors)
 from .functional import (false_predicates,
                          predicates,
                          true_predicates)
 from .literals import (keywords_arguments,
                        objects,
                        positionals_arguments)
-from .definitions import (built_in_functions,
-                          functions,
-                          methods,
-                          methods_descriptors)
