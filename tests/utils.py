b6efd49f0d6d5fdebc24a4fd3ed99ae8c686d90b
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-05 16:54:26 +0300

    Complete 'find' utility

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,33 @@
 from typing import Any
 
-from hypothesis import core
+from hypothesis import (Phase,
+                        core,
+                        settings)
+from hypothesis.errors import (NoSuchExample,
+                               Unsatisfiable)
 from hypothesis.searchstrategy import SearchStrategy
 
 
 def find(strategy: SearchStrategy) -> Any:
-    return core.find(strategy,
-                     lambda _: True)
+    first_object_list = []
+
+    def condition(object_: Any) -> bool:
+        if first_object_list:
+            return True
+        else:
+            first_object_list.append(object_)
+            return False
+
+    try:
+        return core.find(strategy,
+                         condition,
+                         settings=settings(database=None,
+                                           phases=tuple(set(Phase)
+                                                        - {Phase.shrink})))
+    except (NoSuchExample, Unsatisfiable) as search_error:
+        try:
+            result, = first_object_list
+        except ValueError as unpacking_error:
+            raise unpacking_error from search_error
+        else:
+            return result
