351dc84cd63cb8f155d28a804de25cf9998bada7
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-05 17:49:38 +0300

    Complete objects names

--- tests/functional_tests/test_negate.py
+++ tests/functional_tests/test_negate.py
@@ -10,35 +10,37 @@ def test_basic(false_predicate: Predicate,
                true_predicate: Predicate,
                positional_arguments: Tuple,
                keyword_arguments: Dict[str, Any]) -> None:
-    non_false_predicate = negate(false_predicate)
-    non_true_predicate = negate(true_predicate)
+    negated_false_predicate = negate(false_predicate)
+    negated_true_predicate = negate(true_predicate)
 
-    non_false_predicate_result = non_false_predicate(*positional_arguments,
-                                                     **keyword_arguments)
-    non_true_predicate_result = non_true_predicate(*positional_arguments,
-                                                   **keyword_arguments)
+    negated_false_predicate_result = negated_false_predicate(
+            *positional_arguments,
+            **keyword_arguments)
+    negated_true_predicate_result = negated_true_predicate(
+        *positional_arguments,
+        **keyword_arguments)
 
-    assert isinstance(non_false_predicate_result, bool)
-    assert isinstance(non_true_predicate_result, bool)
-    assert non_false_predicate_result
-    assert not non_true_predicate_result
+    assert isinstance(negated_false_predicate_result, bool)
+    assert isinstance(negated_true_predicate_result, bool)
+    assert negated_false_predicate_result
+    assert not negated_true_predicate_result
 
 
 def test_involution(false_predicate: Predicate,
                     true_predicate: Predicate,
                     positional_arguments: Tuple,
                     keyword_arguments: Dict[str, Any]) -> None:
-    non_non_false_predicate = negate(negate(false_predicate))
-    non_non_true_predicate = negate(negate(true_predicate))
+    double_negated_false_predicate = negate(negate(false_predicate))
+    double_negated_true_predicate = negate(negate(true_predicate))
 
-    non_non_false_predicate_result = non_non_false_predicate(
+    double_negated_false_predicate_result = double_negated_false_predicate(
             *positional_arguments,
             **keyword_arguments)
-    non_non_true_predicate_result = non_non_true_predicate(
+    double_negated_true_predicate_result = double_negated_true_predicate(
             *positional_arguments,
             **keyword_arguments)
 
-    assert isinstance(non_non_false_predicate_result, bool)
-    assert isinstance(non_non_true_predicate_result, bool)
-    assert not non_non_false_predicate_result
-    assert non_non_true_predicate_result
+    assert isinstance(double_negated_false_predicate_result, bool)
+    assert isinstance(double_negated_true_predicate_result, bool)
+    assert not double_negated_false_predicate_result
+    assert double_negated_true_predicate_result
