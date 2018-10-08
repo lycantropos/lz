2863b9ac7b4634a78119fa3b0534352710fc6d4c
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-08 20:20:29 +0300

    Add test for 'left.accumulator' function factory

---
+++ tests/left_tests/test_accumulator.py
@@ -0,0 +1,33 @@
+from itertools import tee
+from typing import (Any,
+                    Callable,
+                    Iterable)
+
+from lz import left
+from lz.hints import (Domain,
+                      Range)
+from lz.iterating import (first,
+                          last)
+
+
+def test_first(projector: Callable[[Range, Domain], Range],
+               projector_initial: Range,
+               projector_iterable: Iterable[Domain]) -> None:
+    accumulate = left.accumulator(projector, projector_initial)
+
+    result = accumulate(projector_iterable)
+
+    assert first(result) is projector_initial
+
+
+def test_last(projector: Callable[[Range, Domain], Range],
+              projector_initial: Range,
+              projector_iterable: Iterable[Domain]) -> None:
+    first_target, second_target = tee(projector_iterable)
+    accumulate = left.accumulator(projector, projector_initial)
+    fold = left.folder(projector, projector_initial)
+
+    result = accumulate(first_target)
+    fold_result = fold(second_target)
+
+    assert last(result) == fold_result
