17969a9a8a2cdccd44844ab4f4974c4c810b634f
Ibrakov Azat <azatibrakov@gmail.com>
2018-10-19 20:30:11 +0300

    Complete supported/unsupported objects

--- tests/strategies/definitions.py
+++ tests/strategies/definitions.py
@@ -1,27 +1,8 @@
-import _ast
-import _collections
-import _collections_abc
-import _hashlib
-import _io
-import _string
-import _thread
-import _weakrefset
-import codecs
-import collections
-import ctypes.util
-import encodings
-import faulthandler
+import importlib
 import inspect
-import itertools
 import os
 import platform
-import random
-import socket
-import struct
 import sys
-import types
-import warnings
-import zipimport
 from operator import methodcaller
 from pathlib import Path
 from types import (BuiltinFunctionType,
@@ -41,10 +22,11 @@ def find_stdlib_modules_names(directory_path: Path = Path(os.__file__).parent,
     yield from sys.builtin_module_names
 
     def is_stdlib_module_path(path: Path) -> bool:
-        file_name = path.name
-        # skips '__pycache__', 'site-packages', etc.
-        return not (file_name.startswith('__') and file_name.endswith('__')
-                    or '-' in file_name)
+        base_name = path.stem
+        # skips 'LICENSE', '__pycache__', 'site-packages', etc.
+        return not (base_name.isupper()
+                    or base_name.startswith('__')
+                    or '-' in base_name)
 
     sources_paths = filter(is_stdlib_module_path, directory_path.iterdir())
     sources_relative_paths = map(methodcaller(Path.relative_to.__name__,
@@ -55,19 +37,45 @@ def find_stdlib_modules_names(directory_path: Path = Path(os.__file__).parent,
 
 
 stdlib_modules_names = frozenset(find_stdlib_modules_names())
-stdlib_modules = {module
-                  for module_name, module in sys.modules.items()
-                  if module_name in stdlib_modules_names}
-supported_modules = (stdlib_modules
-                     # support for these stdlib modules is not found
-                     # in ``typeshed`` package
-                     - {_string,
-                        ctypes,
-                        faulthandler})
+stdlib_modules = list(map(importlib.import_module, stdlib_modules_names))
+supported_modules = set(stdlib_modules)
+unsupported_modules = set()
+
+if platform.python_implementation() != 'PyPy':
+    import _lsprof
+    import _multibytecodec
+    import _string
+    import aifc
+    import audioop
+    import bdb
+    import ctypes
+    import faulthandler
+    import mailbox
+    import parser
+    import turtle
+    import unittest
+    import xxsubtype
+
+    # not supported by ``typeshed`` package
+    unsupported_modules.update({_lsprof,
+                                _multibytecodec,
+                                _string,
+                                aifc,
+                                audioop,
+                                bdb,
+                                ctypes,
+                                faulthandler,
+                                mailbox,
+                                parser,
+                                turtle,
+                                unittest,
+                                xxsubtype})
+
+supported_modules -= unsupported_modules
 
 is_module_supported = supported_modules.__contains__
 
-modules = (strategies.sampled_from(list(stdlib_modules))
+modules = (strategies.sampled_from(stdlib_modules)
            .filter(is_module_supported))
 
 
@@ -90,40 +98,89 @@ def is_not_private(object_: Union[BuiltinFunctionType,
     return not object_.__name__.startswith('_')
 
 
-unsupported_classes = {_ast.excepthandler,
-                       _collections_abc.mappingproxy,
-                       _collections_abc.dict_keys,
-                       _collections_abc.dict_items,
-                       _collections_abc.range_iterator,
-                       _thread.LockType,
-                       _thread.RLock,
-                       _thread._local,
-                       _io._IOBase,
-                       _io._RawIOBase,
-                       _io._TextIOBase,
-                       _io._BufferedIOBase,
-                       itertools._tee,
-                       itertools._tee_dataobject,
-                       _weakrefset.ref,
-                       encodings.CodecRegistryError,
-                       random._MethodType,
-                       struct.Struct,
-                       types.CodeType,
-                       types.FrameType,
-                       types.ModuleType,
-                       warnings._OptionError}
+unsupported_classes = set()
 
 if platform.python_implementation() != 'PyPy':
+    import _ast
+    import _collections
+    import _collections_abc
+    import _hashlib
+    import _io
+    import _thread
+    import itertools
     import _json
+    import _ssl
+    import _weakrefset
+    import encodings
+    import macpath
+    import pdb
+    import plistlib
+    import random
+    import runpy
+    import smtplib
+    import socket
+    import struct
+    import tarfile
+    import tkinter
+    import types
+    import warnings
 
-    unsupported_classes.update({_collections._deque_iterator,
+    # not supported by ``typeshed`` package
+    unsupported_classes.update({_ast.excepthandler,
+                                _collections._deque_iterator,
                                 _collections._deque_reverse_iterator,
+                                _collections_abc.dict_items,
+                                _collections_abc.dict_keys,
+                                _collections_abc.mappingproxy,
+                                _collections_abc.range_iterator,
                                 _hashlib.HASH,
-                                _json.make_scanner,
+                                _io._BufferedIOBase,
+                                _io._IOBase,
+                                _io._RawIOBase,
+                                _io._TextIOBase,
                                 _json.make_encoder,
-                                itertools._grouper})
-if sys.platform == 'win32':
-    unsupported_classes.add(os.statvfs_result)
+                                _json.make_scanner,
+                                _ssl._SSLContext,
+                                _thread.LockType,
+                                _thread.RLock,
+                                _thread._local,
+                                _weakrefset.ref,
+                                itertools._grouper,
+                                itertools._tee,
+                                itertools._tee_dataobject,
+                                encodings.CodecRegistryError,
+                                macpath.norm_error,
+                                os.terminal_size,
+                                os.times_result,
+                                os.uname_result,
+                                plistlib._InternalDict,
+                                pdb._rstr,
+                                pdb.Restart,
+                                random._MethodType,
+                                runpy._Error,
+                                smtplib.SMTPNotSupportedError,
+                                socket._GiveupOnSendfile,
+                                struct.Struct,
+                                tarfile.EmptyHeaderError,
+                                tarfile.EOFHeaderError,
+                                tarfile.InvalidHeaderError,
+                                tarfile.TruncatedHeaderError,
+                                tarfile.SubsequentHeaderError,
+                                tkinter.TclError,
+                                types.CodeType,
+                                types.FrameType,
+                                types.ModuleType,
+                                warnings._OptionError})
+    if sys.platform == 'win32':
+        unsupported_classes.update({os.statvfs_result})
+    else:
+        import pwd
+        import termios
+
+        unsupported_classes.update({os.sched_param,
+                                    os.waitid_result,
+                                    pwd.struct_passwd,
+                                    termios.error})
 
 
 def is_class_supported(class_: type) -> bool:
@@ -141,35 +198,48 @@ def is_method_descriptor(object_: Any) -> bool:
     return isinstance(object_, MethodDescriptorType)
 
 
-unsupported_methods_descriptors = {dict.get,
-                                   int.conjugate,
-                                   float.conjugate,
-                                   zipimport.zipimporter.find_loader,
-                                   _io.BufferedRWPair.peek,
-                                   _collections_abc.generator.send,
-                                   _collections_abc.generator.throw,
-                                   _collections_abc.generator.close,
-                                   _collections_abc.coroutine.send,
-                                   _collections_abc.coroutine.throw,
-                                   _collections_abc.coroutine.close,
-                                   collections.OrderedDict.clear,
-                                   collections.OrderedDict.pop,
-                                   collections.OrderedDict.update,
-                                   collections.OrderedDict.setdefault}
-if sys.version_info >= (3, 6):
-    unsupported_methods_descriptors.update(
-            {_collections_abc.async_generator.asend,
-             _collections_abc.async_generator.athrow,
-             _collections_abc.async_generator.aclose})
-    if sys.platform == 'linux':
-        unsupported_methods_descriptors.add(socket.socket.sendmsg_afalg)
-if sys.version_info >= (3, 7):
-    unsupported_methods_descriptors.update({bytearray.isascii,
-                                            bytes.isascii,
-                                            str.isascii,
-                                            socket.socket.getblocking})
-if sys.platform == 'win32' and platform.python_implementation() != 'PyPy':
-    unsupported_methods_descriptors.add(socket.socket.share)
+unsupported_methods_descriptors = set()
+
+if platform.python_implementation() != 'PyPy':
+    import collections
+    import zipimport
+
+    # not supported by ``typeshed`` package
+    unsupported_methods_descriptors.update({_collections_abc.generator.close,
+                                            _collections_abc.generator.send,
+                                            _collections_abc.generator.throw,
+                                            _collections_abc.coroutine.close,
+                                            _collections_abc.coroutine.send,
+                                            _collections_abc.coroutine.throw,
+                                            _io.BufferedRWPair.peek,
+                                            collections.OrderedDict.clear,
+                                            collections.OrderedDict.pop,
+                                            collections.OrderedDict.setdefault,
+                                            collections.OrderedDict.update,
+                                            dict.get,
+                                            int.conjugate,
+                                            float.conjugate,
+                                            zipimport.zipimporter.find_loader})
+    if sys.version_info >= (3, 6):
+        unsupported_methods_descriptors.update(
+                {_collections_abc.async_generator.aclose,
+                 _collections_abc.async_generator.asend,
+                 _collections_abc.async_generator.athrow})
+        if sys.platform == 'linux':
+            import socket
+
+            unsupported_methods_descriptors.add(socket.socket.sendmsg_afalg)
+    if sys.version_info >= (3, 7):
+        import socket
+
+        unsupported_methods_descriptors.update({bytes.isascii,
+                                                bytearray.isascii,
+                                                socket.socket.getblocking,
+                                                str.isascii})
+    if sys.platform == 'win32':
+        import socket
+
+        unsupported_methods_descriptors.add(socket.socket.share)
 
 
 def is_method_descriptor_supported(method_descriptor: MethodDescriptorType
@@ -182,44 +252,48 @@ methods_descriptors = (classes_objects.filter(is_method_descriptor)
                        .filter(is_method_descriptor_supported))
 functions = objects.filter(inspect.isfunction)
 
-# support for these built-in functions in ``typeshed`` package is not found
-unsupported_built_in_functions = {_hashlib.openssl_sha1,
-                                  _hashlib.openssl_sha224,
-                                  _hashlib.openssl_sha256,
-                                  _hashlib.openssl_sha384,
-                                  _hashlib.openssl_sha512,
-                                  _hashlib.openssl_md5,
-                                  socket.dup,
-                                  sys.callstats,
-                                  sys.set_coroutine_wrapper,
-                                  sys.get_coroutine_wrapper,
-                                  _thread.start_new_thread,
-                                  _thread.allocate,
-                                  _thread.exit_thread,
-                                  _thread.interrupt_main,
-                                  _thread.stack_size,
-                                  os.get_inheritable,
-                                  os.set_inheritable,
-                                  # FIXME: next functions meta-info
-                                  # seems to be broken
-                                  codecs.strict_errors,
-                                  codecs.ignore_errors,
-                                  codecs.replace_errors,
-                                  codecs.xmlcharrefreplace_errors,
-                                  codecs.backslashreplace_errors,
-                                  codecs.namereplace_errors}
-if sys.version_info >= (3, 6):
-    unsupported_built_in_functions.update({sys.getfilesystemencodeerrors,
-                                           sys.set_asyncgen_hooks,
-                                           sys.get_asyncgen_hooks})
-if sys.platform == 'win32' and platform.python_implementation() != 'PyPy':
-    unsupported_built_in_functions.update({os.get_handle_inheritable,
-                                           os.set_handle_inheritable})
-if platform.python_implementation() != 'PyPy':
-    import _json
+unsupported_built_in_functions = set()
 
-    unsupported_built_in_functions.update({sys.getallocatedblocks,
-                                           _json.encode_basestring})
+if platform.python_implementation() != 'PyPy':
+    import _hashlib
+    import _json
+    import _thread
+    import codecs
+    import socket
+
+    # not supported by ``typeshed`` package
+    unsupported_built_in_functions.update({_hashlib.openssl_md5,
+                                           _hashlib.openssl_sha1,
+                                           _hashlib.openssl_sha224,
+                                           _hashlib.openssl_sha256,
+                                           _hashlib.openssl_sha384,
+                                           _hashlib.openssl_sha512,
+                                           _json.encode_basestring,
+                                           _thread.allocate,
+                                           _thread.exit_thread,
+                                           _thread.interrupt_main,
+                                           _thread.stack_size,
+                                           _thread.start_new_thread,
+                                           codecs.backslashreplace_errors,
+                                           codecs.ignore_errors,
+                                           codecs.namereplace_errors,
+                                           codecs.replace_errors,
+                                           codecs.strict_errors,
+                                           codecs.xmlcharrefreplace_errors,
+                                           os.get_inheritable,
+                                           os.set_inheritable,
+                                           socket.dup,
+                                           sys.callstats,
+                                           sys.getallocatedblocks,
+                                           sys.get_coroutine_wrapper,
+                                           sys.set_coroutine_wrapper})
+    if sys.version_info >= (3, 6):
+        unsupported_built_in_functions.update({sys.getfilesystemencodeerrors,
+                                               sys.get_asyncgen_hooks,
+                                               sys.set_asyncgen_hooks})
+    if sys.platform == 'win32':
+        unsupported_built_in_functions.update({os.get_handle_inheritable,
+                                               os.set_handle_inheritable})
 
 
 def is_function_supported(function: BuiltinFunctionType) -> bool:
