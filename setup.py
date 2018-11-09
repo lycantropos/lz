af3fed53f97d75c1ecf1e1c5f900e35e2bee1d4a
Ibrakov Azat <azatibrakov@gmail.com>
2018-11-09 14:23:48 +0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import lz
 project_base_url = 'https://github.com/lycantropos/lz/'
 
 install_requires = [
-    'paradigm>=0.0.2',
+    'paradigm>=0.0.3',
     'typing_extensions>=3.6.5',
 ]
 setup_requires = [
