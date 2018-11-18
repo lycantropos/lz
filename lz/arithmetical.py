fce826170107f974bb573fd75a547d040100aebf
Ibrakov Azat <azatibrakov@gmail.com>
2018-11-18 19:55:42 +0300

    Complete signature, add docstring

--- lz/arithmetical.py
+++ lz/arithmetical.py
@@ -1,2 +1,8 @@
-def ceil_division(left_integer: int, right_integer: int) -> int:
-    return -(-left_integer // right_integer)
+from numbers import Real
+
+
+def ceil_division(left_number: Real, right_number: Real) -> Real:
+    """
+    Divides given numbers with ceiling.
+    """
+    return -(-left_number // right_number)
