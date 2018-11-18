c4032948db408ae137933a05f4e0ed340b4aa211
Ibrakov Azat <azatibrakov@gmail.com>
2018-11-18 12:37:18 +0300

    Add docstrings

--- lz/textual.py
+++ lz/textual.py
@@ -11,6 +11,9 @@ def split(string: AnyStr,
           *,
           separator: AnyStr,
           keep_separator: bool) -> List[AnyStr]:
+    """
+    Splits given string by given separator.
+    """
     parts = string.split(separator)
     if keep_separator:
         *parts, last_part = parts
@@ -21,11 +24,19 @@ def split(string: AnyStr,
 
 
 def decoder(encoding: str) -> Map[bytes, str]:
+    """
+    Returns function that decodes byte sequence
+    with codec registered for given encoding.
+    """
     return partial(codecs.decode,
                    encoding=encoding)
 
 
 def encoder(encoding: str) -> Map[str, bytes]:
+    """
+    Returns function that encodes string
+    with codec registered for given encoding.
+    """
     return partial(codecs.encode,
                    encoding=encoding)
 
@@ -34,6 +45,9 @@ def read_batch(stream: IO[AnyStr],
                *,
                batch_size: int,
                remaining_bytes_count: int) -> AnyStr:
+    """
+    Reads batch from given stream.
+    """
     if remaining_bytes_count > batch_size:
         offset = remaining_bytes_count - batch_size
     else:
