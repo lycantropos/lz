fac79f546b7ab31b7c1e5073a6d55fa78798731a
Ibrakov Azat <azatibrakov@gmail.com>
2018-12-14 21:51:19 +0300

    Add test for 'interleave' function

---
+++ tests/iterating_tests/test_interleave.py
@@ -0,0 +1,26 @@
+from itertools import tee
+from typing import (Any,
+                    Iterable)
+
+from lz import left
+from lz.iterating import (first,
+                          interleave)
+from tests.utils import are_iterables_similar
+
+
+def test_basic(empty_iterable: Iterable[Any],
+               nested_iterable: Iterable[Iterable[Any]]) -> None:
+    original, target = tee(nested_iterable)
+
+    result = interleave(left.attacher(empty_iterable)(target))
+
+    assert are_iterables_similar(result, interleave(original))
+
+
+def test_step(non_empty_iterable: Iterable[Any],
+              nested_iterable: Iterable[Iterable[Any]]) -> None:
+    original, target = tee(non_empty_iterable)
+
+    result = interleave(left.attacher(target)(nested_iterable))
+
+    assert first(result) == first(original)
