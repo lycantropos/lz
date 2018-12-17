9b3162963c382430705d9bbb78fa41654e90d8e6
Ibrakov Azat <azatibrakov@gmail.com>
2018-12-17 10:45:27 +0300

    Complete 'slider' function factory test

--- tests/iterating_tests/test_slider.py
+++ tests/iterating_tests/test_slider.py
@@ -1,29 +1,101 @@
-from itertools import islice
 from typing import (Any,
                     Iterable)
 
-import pytest
-
-from lz.iterating import slider
+from lz import (left,
+                right)
+from lz.iterating import (first,
+                          last,
+                          slider)
 from lz.replication import duplicate
+from tests.utils import (capacity,
+                         is_empty)
 
 
-def test_basic(iterable: Iterable[Any],
-               slider_size: int) -> None:
-    original, target = duplicate(iterable)
+def test_base_case_capacity(empty_iterable: Iterable[Any],
+                            slider_size: int) -> None:
     slide = slider(slider_size)
 
-    result = slide(target)
-    result_iterator = iter(result)
+    result = slide(empty_iterable)
 
-    original_elements = tuple(islice(original, slider_size))
-    assert original_elements == next(result_iterator)
-    for result_element, original_element in zip(result_iterator, original):
-        original_elements = original_elements[1:] + (original_element,)
-        assert result_element == original_elements
+    assert capacity(result) == 1
 
-    with pytest.raises(StopIteration):
-        next(original)
 
-    with pytest.raises(StopIteration):
-        next(result_iterator)
+def test_base_case_elements(empty_iterable: Iterable[Any],
+                            slider_size: int) -> None:
+    slide = slider(slider_size)
+
+    result = slide(empty_iterable)
+
+    assert all(map(is_empty, result))
+
+
+def test_step_left_total_capacity(iterable: Iterable[Any],
+                                  object_: Any,
+                                  slider_size: int) -> None:
+    original, target = duplicate(iterable)
+    slide = slider(slider_size)
+    attach = left.attacher(object_)
+
+    result = slide(attach(target))
+
+    assert 0 <= capacity(result) - capacity(slide(original)) <= 1
+
+
+def test_step_left_elementwise_capacity(iterable: Iterable[Any],
+                                        object_: Any,
+                                        slider_size: int) -> None:
+    original, target = duplicate(iterable)
+    slide = slider(slider_size)
+    attach = left.attacher(object_)
+
+    result = slide(attach(target))
+
+    assert all(0 <= capacity(element) <= slider_size
+               for element in result)
+
+
+def test_step_left_elements(iterable: Iterable[Any],
+                            object_: Any,
+                            slider_size: int) -> None:
+    slide = slider(slider_size)
+    attach = left.attacher(object_)
+
+    result = slide(attach(iterable))
+
+    assert first(first(result)) is object_
+
+
+def test_step_right_total_capacity(iterable: Iterable[Any],
+                                   object_: Any,
+                                   slider_size: int) -> None:
+    original, target = duplicate(iterable)
+    slide = slider(slider_size)
+    attach = right.attacher(object_)
+
+    result = slide(attach(target))
+
+    assert 0 <= capacity(result) - capacity(slide(original)) <= 1
+
+
+def test_step_right_elementwise_capacity(iterable: Iterable[Any],
+                                         object_: Any,
+                                         slider_size: int) -> None:
+    original, target = duplicate(iterable)
+    slide = slider(slider_size)
+    attach = right.attacher(object_)
+
+    result = slide(attach(target))
+
+    assert all(0 <= capacity(element) <= slider_size
+               for element in result)
+
+
+def test_step_right_elements(iterable: Iterable[Any],
+                             object_: Any,
+                             slider_size: int) -> None:
+    slide = slider(slider_size)
+    attach = right.attacher(object_)
+
+    result = slide(attach(iterable))
+
+    assert last(last(result)) is object_
