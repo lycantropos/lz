64920af59e489d311b84f33ced1f18b354cf47ac
Ibrakov Azat <azatibrakov@gmail.com>
2018-12-26 19:27:08 +0300

    Complete test for 'right.accumulator' factory

--- tests/right_tests/test_accumulator.py
+++ tests/right_tests/test_accumulator.py
@@ -1,5 +1,5 @@
 from typing import (Callable,
-                    Iterable)
+                    Sequence)
 
 from lz import right
 from lz.hints import (Domain,
@@ -12,18 +12,18 @@ from tests.utils import are_objects_similar
 
 def test_first(projector: Callable[[Domain, Range], Range],
                projector_initial: Range,
-               projector_iterable: Iterable[Domain]) -> None:
+               projector_sequence: Sequence[Domain]) -> None:
     accumulate = right.accumulator(projector, projector_initial)
 
-    result = accumulate(projector_iterable)
+    result = accumulate(projector_sequence)
 
     assert first(result) is projector_initial
 
 
 def test_last(projector: Callable[[Domain, Range], Range],
               projector_initial: Range,
-              projector_iterable: Iterable[Domain]) -> None:
-    first_target, second_target = duplicate(projector_iterable)
+              projector_sequence: Sequence[Domain]) -> None:
+    first_target, second_target = duplicate(projector_sequence)
     accumulate = right.accumulator(projector, projector_initial)
     fold = right.folder(projector, projector_initial)
 
