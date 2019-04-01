8afffd394be72329b8774d110c5485ce0ac767f9
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-01 09:25:37 +0300

    Complete test for 'combine' function factory

--- tests/functional_tests/test_combine.py
+++ tests/functional_tests/test_combine.py
@@ -12,9 +12,9 @@ from tests.utils import (are_iterables_similar,
 
 
 def test_base_case(positional_arguments: Tuple[Any, ...]) -> None:
-    combined = combine()
+    combination = combine()
 
-    result = combined(positional_arguments)
+    result = combination(positional_arguments)
 
     assert is_empty(result)
 
@@ -23,11 +23,11 @@ def test_step_left(maps: Sequence[Map],
                    map_: Map,
                    maps_arguments: Sequence[Domain],
                    map_argument: Domain) -> None:
-    combined = combine(*maps)
-    next_combined = combine(*left.attacher(map_)(maps))
+    combination = combine(*maps)
+    next_combination = combine(*left.attacher(map_)(maps))
 
-    result = combined(maps_arguments)
-    next_result = next_combined(left.attacher(map_argument)(maps_arguments))
+    result = combination(maps_arguments)
+    next_result = next_combination(left.attacher(map_argument)(maps_arguments))
 
     assert are_iterables_similar(next_result,
                                  left.attacher(map_(map_argument))(result))
@@ -37,11 +37,12 @@ def test_step_right(maps: Sequence[Map],
                     map_: Map,
                     maps_arguments: Sequence[Domain],
                     map_argument: Domain) -> None:
-    combined = combine(*maps)
-    next_combined = combine(*right.attacher(map_)(maps))
+    combination = combine(*maps)
+    next_combination = combine(*right.attacher(map_)(maps))
 
-    result = combined(maps_arguments)
-    next_result = next_combined(right.attacher(map_argument)(maps_arguments))
+    result = combination(maps_arguments)
+    next_result = next_combination(right
+                                   .attacher(map_argument)(maps_arguments))
 
     assert are_iterables_similar(next_result,
                                  right.attacher(map_(map_argument))(result))
