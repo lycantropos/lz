5294f1ef2c3eb3830699b4980c8999aa4b368606
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-12 15:15:25 +0300

    Complete test for 'curry' function factory

--- tests/functional_tests/curry_tests/test_properties.py
+++ tests/functional_tests/curry_tests/test_properties.py
@@ -29,6 +29,20 @@ def test_valid_call(transparent_function: Callable[..., Range],
                                           **transparent_function_kwargs)
 
 
+def test_consecutive_call(transparent_function: Callable[..., Range],
+                          transparent_function_args: Tuple[Domain, ...],
+                          transparent_function_kwargs: Dict[str, Domain]
+                          ) -> None:
+    curried = curry(transparent_function)
+    double_curried = curry(curried)
+
+    result = double_curried(*transparent_function_args,
+                            **transparent_function_kwargs)
+
+    assert result == curried(*transparent_function_args,
+                             **transparent_function_kwargs)
+
+
 def test_invalid_args_call(
         non_variadic_transparent_function: Callable[..., Range],
         non_variadic_transparent_function_invalid_args: Tuple[Domain, ...],
