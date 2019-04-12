47080c326114014026e88acbcea271dab11a32e5
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-12 20:46:33 +0300

    Complete test for 'to_constant' function factory

---
+++ tests/functional_tests/to_constant_tests/test_pickle.py
@@ -0,0 +1,20 @@
+from typing import (Any,
+                    Dict,
+                    Tuple)
+
+from lz.functional import to_constant
+from tests.utils import (are_objects_similar,
+                         round_trip_pickle)
+
+
+def test_round_trip(object_: Any,
+                    positional_arguments: Tuple,
+                    keyword_arguments: Dict[str, Any]) -> None:
+    constant = to_constant(object_)
+
+    result = round_trip_pickle(constant)
+
+    assert are_objects_similar(result(*positional_arguments,
+                                      **keyword_arguments),
+                               constant(*positional_arguments,
+                                        **keyword_arguments))
