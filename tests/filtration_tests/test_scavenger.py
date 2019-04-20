fd767b40593775a3c1ac15ae6c51a32f8872947a
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-21 01:35:23 +0300

    Complete test for 'scavenger' function factory

--- tests/filtration_tests/test_scavenger.py
+++ tests/filtration_tests/test_scavenger.py
@@ -2,11 +2,15 @@ from operator import not_
 from typing import (Any,
                     Iterable)
 
+from hypothesis import given
+
 from lz.filtration import scavenger
 from lz.hints import Predicate
 from lz.logical import negate
+from tests import strategies
 
 
+@given(strategies.iterables)
 def test_default_predicate(iterable: Iterable[Any]) -> None:
     scavenge = scavenger()
 
@@ -15,6 +19,7 @@ def test_default_predicate(iterable: Iterable[Any]) -> None:
     assert all(map(not_, result))
 
 
+@given(strategies.iterables, strategies.predicates)
 def test_custom_predicate(iterable: Iterable[Any],
                           predicate: Predicate) -> None:
     scavenge = scavenger(predicate)
