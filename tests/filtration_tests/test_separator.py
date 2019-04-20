3ca3ca76e8ae754ea1534afdb5bccd80a2746f3a
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-21 01:35:35 +0300

    Complete test for 'separator' function factory

--- tests/filtration_tests/test_separator.py
+++ tests/filtration_tests/test_separator.py
@@ -2,11 +2,15 @@ from operator import not_
 from typing import (Any,
                     Iterable)
 
+from hypothesis import given
+
 from lz.filtration import separator
 from lz.hints import Predicate
 from lz.logical import negate
+from tests import strategies
 
 
+@given(strategies.iterables)
 def test_default_predicate(iterable: Iterable[Any]) -> None:
     separate = separator()
 
@@ -16,6 +20,7 @@ def test_default_predicate(iterable: Iterable[Any]) -> None:
     assert all(true_like)
 
 
+@given(strategies.iterables, strategies.predicates)
 def test_custom_predicate(iterable: Iterable[Any],
                           predicate: Predicate) -> None:
     separate = separator(predicate)
