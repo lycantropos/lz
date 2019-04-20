d6b77ca9a0a4af8f2b7dd7c099b35714452d812d
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-21 01:23:39 +0300

    Complete test for 'trailer' function factory

--- tests/iterating_tests/trailer_tests/test_definition.py
+++ tests/iterating_tests/trailer_tests/test_definition.py
@@ -1,12 +1,16 @@
 from typing import (Any,
                     Iterable)
 
+from hypothesis import given
+
 from lz.iterating import (capacity,
                           trailer)
 from lz.replication import duplicate
+from tests import strategies
 from tests.utils import iterable_ends_with
 
 
+@given(strategies.non_empty_iterables, strategies.non_negative_indices)
 def test_capacity(non_empty_iterable: Iterable[Any],
                   size: int) -> None:
     original, target = duplicate(non_empty_iterable)
@@ -20,6 +24,7 @@ def test_capacity(non_empty_iterable: Iterable[Any],
         assert capacity(result) == size
 
 
+@given(strategies.non_empty_iterables, strategies.non_negative_indices)
 def test_elements(non_empty_iterable: Iterable[Any],
                   size: int) -> None:
     original, target = duplicate(non_empty_iterable)
