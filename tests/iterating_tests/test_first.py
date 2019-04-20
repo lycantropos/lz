a6d576e98e3884a98c21dc0fa9d28fe37b454b0a
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-21 01:06:41 +0300

    Complete test for 'first' function

--- tests/iterating_tests/test_first.py
+++ tests/iterating_tests/test_first.py
@@ -1,12 +1,16 @@
 from typing import (Any,
                     Iterable)
 
+from hypothesis import given
+
 from lz.iterating import (expand,
                           first)
 from lz.replication import duplicate
+from tests import strategies
 from tests.utils import iterable_starts_with
 
 
+@given(strategies.non_empty_iterables)
 def test_basic(non_empty_iterable: Iterable[Any]) -> None:
     original, target = duplicate(non_empty_iterable)
 
