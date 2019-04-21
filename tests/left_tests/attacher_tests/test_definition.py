4ea1b34c35c154b26717df0072eb5f0e5789c7c6
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-21 09:15:04 +0300

    Complete test for 'left.attacher' function factory

--- tests/left_tests/attacher_tests/test_definition.py
+++ tests/left_tests/attacher_tests/test_definition.py
@@ -1,12 +1,15 @@
 from typing import (Any,
                     Iterable)
 
+from hypothesis import given
+
 from lz import left
 from lz.iterating import first
+from tests import strategies
 
 
-def test_basic(iterable: Iterable[Any],
-               object_: Any) -> None:
+@given(strategies.iterables, strategies.objects)
+def test_basic(iterable: Iterable[Any], object_: Any) -> None:
     attach = left.attacher(object_)
 
     result = attach(iterable)
