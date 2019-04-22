f8dc8c225b089f1a63d08c157d6803b83b77c671
Ibrakov Azat <azatibrakov@gmail.com>
2019-04-22 08:35:16 +0300

    Complete test for 'right.attacher' function factory

---
+++ tests/right_tests/attacher_tests/test_pickle.py
@@ -0,0 +1,20 @@
+from typing import (Any,
+                    Iterable)
+
+from hypothesis import given
+
+from lz import right
+from lz.replication import duplicate
+from tests import strategies
+from tests.utils import (are_iterables_similar,
+                         round_trip_pickle)
+
+
+@given(strategies.iterables, strategies.objects)
+def test_round_trip(iterable: Iterable[Any], object_: Any) -> None:
+    original, target = duplicate(iterable)
+    attach = right.attacher(object_)
+
+    result = round_trip_pickle(attach)
+
+    assert are_iterables_similar(result(target), attach(original))
