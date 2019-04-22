9c28897c902edc4b60bf59354baab8cbbecb75e8
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-22 13:22:21 +0300

    Complete test utilities

--- tests/utils.py
+++ tests/utils.py
@@ -20,47 +20,16 @@ from typing import (Any,
                     Type)
 
 import pytest
-from hypothesis import (Phase,
-                        core,
-                        settings)
-from hypothesis.errors import (NoSuchExample,
-                               Unsatisfiable)
 
 from lz.hints import (Domain,
                       Map,
                       Range)
 from lz.replication import duplicate
-from .hints import Strategy
 
 try:
     from typing import ContextManager
 except ImportError:
     from typing_extensions import ContextManager
-
-
-def find(strategy: Strategy[Domain]) -> Domain:
-    first_object_list = []
-
-    def condition(object_: Any) -> bool:
-        if first_object_list:
-            return True
-        else:
-            first_object_list.append(object_)
-            return False
-
-    try:
-        return core.find(strategy,
-                         condition,
-                         settings=settings(database=None,
-                                           phases=tuple(set(Phase)
-                                                        - {Phase.shrink})))
-    except (NoSuchExample, Unsatisfiable) as search_error:
-        try:
-            result, = first_object_list
-        except ValueError as unpacking_error:
-            raise unpacking_error from search_error
-        else:
-            return result
 
 
 def iterable_starts_with(iterable: Iterable[Any],
