ea91e1f980ae5d4e226293b157dab82b51d83107
Azat Ibrakov <azatibrakov@gmail.com>
2019-04-23 20:14:56 +0300

    Complete test for 'left.applier' function factory

---
+++ tests/left_tests/applier_tests/test_curry.py
@@ -0,0 +1,43 @@
+import pytest
+from hypothesis import given
+
+from lz import left
+from lz.functional import (compose,
+                           curry)
+from tests import strategies
+from tests.hints import (FunctionCall,
+                         PartitionedFunctionCall)
+
+
+@given(strategies.partitioned_transparent_functions_calls)
+def test_basic(partitioned_function_call: PartitionedFunctionCall) -> None:
+    (function,
+     (first_args_part, second_args_part),
+     (first_kwargs_part, second_kwargs_part)) = partitioned_function_call
+    applied = left.applier(function,
+                           *first_args_part,
+                           **first_kwargs_part)
+
+    result = curry(applied)
+
+    assert (result(*second_args_part, **second_kwargs_part)
+            == function(*first_args_part, *second_args_part,
+                        **first_kwargs_part, **second_kwargs_part))
+
+
+@given(strategies.non_variadic_transparent_functions_calls_with_invalid_args)
+def test_invalid_args(function_call: FunctionCall) -> None:
+    function, invalid_args, kwargs = function_call
+    applied = left.applier(function, *invalid_args, **kwargs)
+
+    with pytest.raises(TypeError):
+        curry(applied)
+
+
+@given(strategies.non_variadic_transparent_functions_calls_with_invalid_kwargs)
+def test_invalid_kwargs(function_call: FunctionCall) -> None:
+    function, args, invalid_kwargs = function_call
+    applied = left.applier(function, *args, **invalid_kwargs)
+
+    with pytest.raises(TypeError):
+        curry(applied)
