6811a4cf9a91a8f86caa9be3a34fe8e6d8ba6239
Ibrakov Azat <azatibrakov@gmail.com>
2019-05-26 01:11:00 +0300

    Complete test for 'compose' function factory

--- tests/functional_tests/compose_tests/strategies.py
+++ tests/functional_tests/compose_tests/strategies.py
@@ -1,12 +1,16 @@
 from hypothesis import strategies
 
 from tests.strategies import (callables,
+                              empty,
                               maps,
                               maps_arguments,
                               various_suitable_maps)
 from tests.strategies.functional import extend_suitable_maps
+from tests.strategies.utils import identifiers
 
 callables = callables
+empty = empty
+identifiers = identifiers
 maps_calls = strategies.tuples(maps, maps_arguments)
 maps_chain_calls = strategies.tuples(various_suitable_maps, maps_arguments)
 maps_triplets_calls = strategies.tuples(
---
+++ tests/functional_tests/compose_tests/test_binding.py
@@ -0,0 +1,34 @@
+from types import MethodType
+from typing import (Tuple,
+                    Type)
+
+from hypothesis import given
+
+from lz.functional import compose
+from lz.hints import Domain
+from tests.hints import CompositionCall
+from . import strategies
+
+
+@given(strategies.identifiers,
+       strategies.empty.tuples,
+       strategies.identifiers,
+       strategies.maps_chain_calls)
+def test_method_definition(class_name: str,
+                           class_bases: Tuple[Type, ...],
+                           method_name: str,
+                           maps_chain_call: CompositionCall) -> None:
+    various_suitable_maps, map_argument = maps_chain_call
+    composition = compose(*various_suitable_maps)
+    method = compose(composition, ignore_self)
+    class_ = type(class_name, class_bases, {method_name: method})
+    instance = class_()
+
+    bound_method = getattr(instance, method_name)
+
+    assert isinstance(bound_method, MethodType)
+    assert bound_method(map_argument) == composition(map_argument)
+
+
+def ignore_self(self, argument: Domain) -> Domain:
+    return argument
