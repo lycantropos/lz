2d55d35e037172ebcf7acd8325dd859ac32e0fa5
Azat Ibrakov <azatibrakov@gmail.com>
2019-05-31 19:20:10 +0300

    Complete docstrings for 'right' module

--- lz/right.py
+++ lz/right.py
@@ -28,6 +28,15 @@ def accumulator(function: Callable[[Domain, Range], Range],
     """
     Returns function that yields cumulative results of given binary function
     starting from given initial object in direction from right to left.
+
+    >>> def to_next_fraction(partial_denominator: int,
+    ...                      reciprocal: float) -> float:
+    ...     return partial_denominator + 1 / reciprocal
+    >>> to_simple_continued_fractions = accumulator(to_next_fraction, 1)
+    >>> from itertools import repeat
+    >>> [round(fraction, 4)
+    ...  for fraction in to_simple_continued_fractions(list(repeat(1, 10)))]
+    [1, 2.0, 1.5, 1.6667, 1.6, 1.625, 1.6154, 1.619, 1.6176, 1.6182, 1.618]
     """
     left_accumulator = left.accumulator(flip(function), initial)
     return compose(left_accumulator, reverse)
@@ -36,6 +45,10 @@ def accumulator(function: Callable[[Domain, Range], Range],
 def attacher(object_: Domain) -> Map[Iterable[Domain], Iterable[Domain]]:
     """
     Returns function that appends given object to iterable.
+
+    >>> attach_hundred = attacher(100)
+    >>> list(attach_hundred(range(10)))
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
     """
     return functools.partial(attach,
                              object_=object_)
@@ -64,6 +77,10 @@ def folder(function: Callable[[Domain, Range], Range],
     """
     Returns function that cumulatively applies given binary function
     starting from given initial object in direction from right to left.
+
+    >>> to_sum_evaluation_order = folder('({} + {})'.format, 0)
+    >>> to_sum_evaluation_order(range(1, 10))
+    '(1 + (2 + (3 + (4 + (5 + (6 + (7 + (8 + (9 + 0)))))))))'
     """
     left_folder = left.folder(flip(function), initial)
     return compose(left_folder, reverse)
@@ -166,6 +183,9 @@ def applier(function: Callable[..., Range],
     Returns function that behaves like given function
     with given arguments partially applied.
     Given positional arguments will be added to the right end.
-    """
 
+    >>> square = applier(pow, 2)
+    >>> square(10)
+    100
+    """
     return Applier(function, *args, **kwargs)
