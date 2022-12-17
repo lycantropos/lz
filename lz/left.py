61786d0cf0419a5980bc608f71c4474fbac47c14
Azat Ibrakov <azatibrakov@gmail.com>
2022-12-18 00:34:35 +0100

    Fix docstrings

--- lz/left.py
+++ lz/left.py
@@ -19,7 +19,7 @@ def accumulator(
 
     >>> import math
     >>> to_pi_approximations = accumulator(round, math.pi)
-    >>> list(to_pi_approximations(_T2(5, 0, -1)))
+    >>> list(to_pi_approximations(range(5, 0, -1)))
     [3.141592653589793, 3.14159, 3.1416, 3.142, 3.14, 3.1]
     """
     return _functools.partial(accumulate, function, initial)
@@ -81,7 +81,7 @@ def folder(function: _t.Callable[[_T2, _T1], _T2],
     starting from given initial object in direction from left to right.
 
     >>> to_sum_evaluation_order = folder('({} + {})'.format, 0)
-    >>> to_sum_evaluation_order(_T2(1, 10))
+    >>> to_sum_evaluation_order(range(1, 10))
     '(((((((((0 + 1) + 2) + 3) + 4) + 5) + 6) + 7) + 8) + 9)'
     """
     return _functools.partial(fold, function, initial)
@@ -105,7 +105,7 @@ def applier(function: _t.Callable[..., _T2],
     with given arguments partially applied.
     Given positional arguments will be added to the left end.
 
-    >>> count_from_zero_to = applier(_T2, 0)
+    >>> count_from_zero_to = applier(range, 0)
     >>> list(count_from_zero_to(10))
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     """
